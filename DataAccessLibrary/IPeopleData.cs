9b7c33a86e00f6167fe1052e7f2d83c6100c8809
jshwang27 <jshwang27@gmail.com>
2020-06-04 00:24:10 +0900

    초기 커밋.

---
+++ DataAccessLibrary/IPeopleData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IPeopleData
+    {
+        Task<List<PersonModel>> GetPeoPle();
+        Task InsertPerson(PersonModel person);
+    }
+}
